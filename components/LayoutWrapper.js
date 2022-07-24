8a99c9383316dc3bb9eb4981c228334a6651ca2c
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-07-24 18:54:50 +0530

    udpate header

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,7 +1,7 @@
 import siteMetadata from '@/data/siteMetadata';
 import headerNavLinks from '@/data/headerNavLinks';
-import Logo from '@/data/logo.svg';
 import Link from './Link';
+import Image from './Image';
 import SectionContainer from './SectionContainer';
 import Footer from './Footer';
 import MobileNav from './MobileNav';
@@ -15,13 +15,20 @@ const LayoutWrapper = ({ children }) => {
 					<div>
 						<Link href="/" aria-label={siteMetadata.headerTitle}>
 							<div className="flex items-center justify-between">
-								<div className="mr-3">
-									<Logo />
+								<div className="mr-3 flex">
+									<Image
+										alt={'site logo'}
+										src={siteMetadata.siteLogo}
+										className="rounded-full object-cover object-center md:h-36 lg:h-48"
+										width={60}
+										height={60}
+									/>
+								</div>
+								<div className="h-8 pr-2 text-3xl font-semibold text-gray-400 dark:text-gray-600">
+									{'/'}
 								</div>
 								{typeof siteMetadata.headerTitle === 'string' ? (
-									<div className="hidden h-6 text-2xl font-semibold sm:block">
-										{siteMetadata.headerTitle}
-									</div>
+									<div className="h-6 text-2xl font-bold">{siteMetadata.headerTitle}</div>
 								) : (
 									siteMetadata.headerTitle
 								)}
