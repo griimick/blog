d5bda330c640b65456c6bc3643c1353a8043fb8d
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-08-27 04:20:41 +0530

    Fix and update content

--- components/DraftBanner.js
+++ components/DraftBanner.js
@@ -5,7 +5,7 @@ export default function DraftBanner() {
 		<div>
 			<span
 				id="draft-banner"
-				className={`fixed left-0 top-0 h-9 w-full bg-primary-500/50 text-center align-middle font-medium leading-9 backdrop-blur transition-transform duration-150`}
+				className={`fixed left-0 top-0 z-50 h-9 w-full bg-primary-500/50 text-center align-middle font-medium leading-9 backdrop-blur transition-transform duration-150`}
 			>
 				You are reading a working draft
 			</span>
--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,12 @@
 import NextImage from 'next/image';
+import { useTheme } from 'next-themes';
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />;
+const Image = ({ srcDark = '', ...rest }) => {
+	const { theme } = useTheme();
+	const src = srcDark && theme === 'dark' ? srcDark : rest.src;
+
+	return <NextImage {...rest} src={src} />;
+};
 
 export default Image;
