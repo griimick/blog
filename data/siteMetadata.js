eade49ce0e6768738fc8ec04d0460e8308728003
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-09-04 01:39:38 +0530

    Set up giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
 	title: 'Blog',
 	author: 'Soumik',
 	headerTitle: 'Blog',
-	description: 'Another attempt to post my notes',
+	description: 'Better drafts end up here',
 	language: 'en-us',
 	theme: 'system', // system, dark or light
 	siteUrl: 'https://blog.soumik.dev',
@@ -38,23 +38,23 @@ const siteMetadata = {
 		// content security policy in the `next.config.js` file.
 		// Select a provider and use the environment variables associated to it
 		// https://vercel.com/docs/environment-variables
-		provider: '', // supported providers: giscus, utterances, disqus
+		provider: 'giscus', // supported providers: giscus, utterances, disqus
 		giscusConfig: {
 			// Visit the link below, and follow the steps in the 'configuration' section
 			// https://giscus.app/
-			repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-			repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-			category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-			categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+			repo: 'griimick/blog',
+			repositoryId: 'R_kgDOHsOpTA',
+			category: 'Announcements',
+			categoryId: 'DIC_kwDOHsOpTM4CRFXh',
 			mapping: 'pathname', // supported options: pathname, url, title
 			reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
 			// Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
 			metadata: '0',
 			// theme example: light, dark, dark_dimmed, dark_high_contrast
 			// transparent_dark, preferred_color_scheme, custom
-			theme: 'light',
+			theme: 'custom',
 			// Place the comment box above the comments. options: bottom, top
-			inputPosition: 'bottom',
+			inputPosition: 'top',
 			// Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
 			lang: 'en',
 			// theme when dark mode
