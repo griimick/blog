7cd3d319b5784a9fdccd8eb0545af983c53950bb
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-09-04 01:12:50 +0530

    Fix image theme not picking user system theme pref.

--- components/Image.js
+++ components/Image.js
@@ -3,8 +3,8 @@ import { useTheme } from 'next-themes';
 
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ srcDark = '', ...rest }) => {
-	const { theme } = useTheme();
-	const src = srcDark && theme === 'dark' ? srcDark : rest.src;
+	const { theme, resolvedTheme } = useTheme();
+	const src = srcDark && (theme === 'dark' || resolvedTheme === 'dark') ? srcDark : rest.src;
 
 	return <NextImage {...rest} src={src} />;
 };
